9bc3c6bdb7c89227c1677f6d042293a7ba4ce019
YuriiR <pelmenevgrad@gmail.com>
2026-07-13 23:57:21 +0300

    isolate app render test

--- frontend/src/screens/MapScreen.jsx
+++ frontend/src/screens/MapScreen.jsx
@@ -116,8 +116,8 @@ export default function MapScreen() {
         ref={mapRef}
       >
         <TileLayer
-          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+          url="https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png"
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>'
         />
 
         {filtered.map((event) => {
